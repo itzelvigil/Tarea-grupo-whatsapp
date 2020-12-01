37ada83329f5aeaa40b840d8a56df14d017acbcb
ItzelVigil <itzel.vigilch@gmail.com>
2020-12-01 00:03:54 -0700

    com2

--- text.py
+++ text.py
@@ -11,23 +11,22 @@ import regex
 from collections import Counter
 
 def startsWithDateAndTime(s):
-    # regex pattern for date.(Works only for android. IOS Whatsapp export format is different. Will update the code soon
     pattern = r'^\d{1,2}/\d{1,2}/\d{2} \d{1,2}:\d{2}'
     result = re.match(pattern, s)
     if result:
         return True
     return False
   
-# Finds username of any given format.
+
 def FindAuthor(s):
     patterns = [
-        '([\w]+):',                        # First Name
-        '([\w]+[\s]+[\w]+):',              # First Name + Last Name
-        '([\w]+[\s]+[\w]+[\s]+[\w]+):',    # First Name + Middle Name + Last Name
+        '([\w]+):',                       
+        '([\w]+[\s]+[\w]+):',             
+        '([\w]+[\s]+[\w]+[\s]+[\w]+):',    
         '([+]\d{2} \d{5} \d{5}):',         
         '([+]\d{2} \d{3} \d{3} \d{4}):',   
         '([\w]+)[\u263a-\U0001f999]+:',
-        '([\w]+) [^\s\u1f300-\u1f5ff]*'    # Name and Emoji              
+        '([\w]+) [^\s\u1f300-\u1f5ff]*'               
     ]
     pattern = '^' + '|'.join(patterns)
     result = re.match(pattern, s)
@@ -48,11 +47,11 @@ def getDataPoint(line):
         author = None
     return date, time, author, message
 
-parsedData = [] # List to keep track of data so it can be used by a Pandas dataframe
-# Upload your file here
+parsedData = [] 
+
 conversationPath = 'chat.txt' # chat file
 with open(conversationPath, encoding="utf-8") as fp:    
-    fp.readline() # Skipping first line of the file because contains information related to something about end-to-end encryption
+    fp.readline() 
     messageBuffer = [] 
     date, time, author = None, None, None
     while True:
@@ -69,7 +68,7 @@ with open(conversationPath, encoding="utf-8") as fp:
         else:
             messageBuffer.append(line)
    
-df = pd.DataFrame(parsedData, columns=['Date', 'Time', 'Author', 'Message']) # Initialising a pandas Dataframe.
+df = pd.DataFrame(parsedData, columns=['Date', 'Time', 'Author', 'Message']) 
 df["Date"] = pd.to_datetime(df["Date"])
 df['Message'] = df['Message'].str.lower() # Mensajes en minúsculas
 df['Message'] = df.Message.str.replace(r"(a|j)?(ja)+(a|j)?", "jaja") 
@@ -126,22 +125,15 @@ print("******************************** \n")
 l = messages_df.Author.unique()
 
 for i in range(len(l)):
-  # Filtering out messages of particular user
   req_df= messages_df[messages_df["Author"] == l[i]]
-  # req_df will contain messages of only one particular user
   print(f'Estadisticas de: {l[i]} -')
-  # shape will print number of rows which indirectly means the number of messages
   print('Mensajes enviados', req_df.shape[0])
-  #Word_Count contains of total words in one message. Sum of all words/ Total Messages will yield words per message
   words_per_message = (np.sum(req_df['Word_Count']))/req_df.shape[0]
   print('Palabras por mensaje ', words_per_message)
-  #media conists of media messages
   media = media_messages_df[media_messages_df['Author'] == l[i]].shape[0]
   print('Mensajes con archivos enviados ', media)
-  # emojis conists of total emojis
   emojis = sum(req_df['emoji'].str.len())
   print('Emojis enviados', emojis)
-  #links consist of total links
   links = sum(req_df["urlcount"])   
   print('Links enviados ', links)   
   print()
